9de161194597d5b5916be8a281bc40b3a6b7d729
ilhamshh-bpa203-dev <ilhamshh-bpa203@code.edu.az>
2025-10-16 18:35:42 +0400

    task 6 added

--- 07-HeapStackRefOutArrayResize/obj/Release/net8.0/task7.AssemblyInfo.cs
+++ 07-HeapStackRefOutArrayResize/obj/Release/net8.0/task7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbaad8d97ab6fc37fa9bcb42ce3f19cc212122b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("task7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ task 6/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cumleni daxil et:");
+            string text = Console.ReadLine();
+            Find(text);
+            
+        }
+        public static void Find(string name)
+        {
+            string soz = "";
+            string uzunsoz = "";
+            for (int i = 0; i < name.Length; i++) 
+            {
+                if (name[i] != ' ')
+                {
+                    soz += name[i];
+                }
+                else
+                {
+                    if (soz.Length > uzunsoz.Length)
+                    {
+                        uzunsoz = soz;
+                    }
+                    soz = "";
+                }
+            }
+            if (soz.Length > uzunsoz.Length)
+            {
+
+                uzunsoz = soz;
+            }
+            Console.WriteLine(uzunsoz);
+        }
+    }
+        
+
+    }
+}
+
+
+
+
+
+
+
+
