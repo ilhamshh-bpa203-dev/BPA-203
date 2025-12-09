8460e3fe787c390dc2f40569590cdcbc951c0ecf
ilhamshh-bpa203-dev <ilhamshh-bpa203@code.edu.az>
2025-12-09 08:19:33 +0400

    mvc intro added

---
+++ 32-MVC Intro/32-WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace _32_WebApplication.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult? Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof (Error));
+            }
+
+            return RedirectToAction("Index","Product");
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 32-MVC Intro/32-WebApplication/Controllers/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _32_WebApplication.Controllers
+{
+    public class Product:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
